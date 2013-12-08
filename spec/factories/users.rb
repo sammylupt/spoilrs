88e25795edf2c367b56d2eed4e035d8387b4bcec
Scott Luptowski <scottluptowski@gmail.com>
2013-12-08 14:54:15 -0500

    faker, and using it for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,11 @@
+require 'faker'
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
   factory :user do
-    name "MyString"
-    screen_name "MyString"
-    tid 1
+    name { Faker::Name.first_name }
+    screen_name { Faker::Internet.user_name }
+    tid { rand(0.1000) }
     token "MyString"
     secret "MyString"
   end
