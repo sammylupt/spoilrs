bd0561229b5d08cbacd2a0c12adc846e47a43a6b
Scott Luptowski <scottluptowski@gmail.com>
2013-12-08 16:07:07 -0500

    better routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 SpoilrsRails::Application.routes.draw do
 
   root 'static#index'
+  get "new" => "posts#new", :as => 'new_post'
+  get ":id/replies/new" => "posts#reply", :as => 'new_reply'
+  get ':id' => "posts#show", :as => 'post'
+  post 'posts' => "posts#create", :as => 'posts'
+
   get "/auth/twitter/callback" => "sessions#create"
   get "/logout" => "sessions#destroy"
 
   get "sessions/new"
-  resources :posts
-  resources :users
 end
