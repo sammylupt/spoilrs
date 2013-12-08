bafb2d9169201c86836c375ab9ff11b7017627e8
Scott Luptowski <scottluptowski@gmail.com>
2013-12-08 15:55:27 -0500

    diff variable names

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
   has_many :posts
 
   def self.find_or_create_by_omniauth(hash)
-    token = hash.credentials.token
-    secret = hash.credentials.secret
+    user_token = hash.credentials.token
+    user_secret = hash.credentials.secret
     #TODO: build in what happens if a Twitter user changes their name
     # The token and secret will stay the same, but the other stuff may change
-    user = User.find_or_create_by(token: token, secret: secret)
+    user = User.find_or_create_by(token: user_token, secret: user_secret)
 
     # TODO: refactor this stuff
     user.tid = hash.uid
