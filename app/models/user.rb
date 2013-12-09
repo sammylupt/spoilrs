657467dd22e8a9e3c946770cdf0011c33055c0bf
Scott Luptowski <scottluptowski@gmail.com>
2013-12-08 23:26:12 -0500

    no variable assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
     secret = hash["credentials"]["secret"]
     tid = hash["uid"]
 
-    user = User.find_or_create_by(token: token, secret: secret, tid: tid) do |u|
+    User.find_or_create_by(token: token, secret: secret, tid: tid) do |u|
       #these bits can change, so write them each login
       u.name = hash["info"]["name"]
       u.screen_name = hash["info"]["nickname"]
