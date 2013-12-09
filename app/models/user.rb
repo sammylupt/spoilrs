ea6a68f831f6840b110256c5d58bcddec69f50df
Scott Luptowski <scottluptowski@gmail.com>
2013-12-08 23:13:08 -0500

    change find or create by omniauth method to make it testable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,17 @@ class User < ActiveRecord::Base
   has_many :posts
 
   def self.find_or_create_by_omniauth(hash)
-    user_token = hash.credentials.token
-    user_secret = hash.credentials.secret
+    binding.pry
+    user_token = hash["credentials"]["token"]
+    user_secret = hash["credentials"]["secret"]
     #TODO: build in what happens if a Twitter user changes their name
     # The token and secret will stay the same, but the other stuff may change
     user = User.find_or_create_by(token: user_token, secret: user_secret)
 
     # TODO: refactor this stuff
-    user.tid = hash.uid
-    user.name = hash.info.name
-    user.screen_name = hash.info.nickname
+    user.tid = hash["uid"]
+    user.name = hash["info"]["name"]
+    user.screen_name = hash["info"]["nickname"]
     user.save
     user
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,28 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:omniauth_hash) { 
+
+    {
+      "credentials" => {
+        "token" => "Test Token",
+        "secret" => "Test Secret"
+      },
+      "uid" => 100,
+      "info" => {
+        "name" => "Test Name",
+        "nickname" => "Test Nickname"
+      }
+    }
+  }
+
+  context "find or create by omniauth" do 
+
+    it "is created by omniauth" do 
+      user = User.find_or_create_by_omniauth(omniauth_hash)
+      expect(User.first).to eq user
+    end
+  end
 
 end
