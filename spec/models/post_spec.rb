97d31ea310026213e4dd54d7ff93fdb0e5b3e0e6
Scott Luptowski <scottluptowski@gmail.com>
2013-12-08 23:02:06 -0500

    model method specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,32 +2,48 @@ require 'spec_helper'
 
 describe Post do
 
-  WebMock.stub_request(:any, "https://api.twitter.com").to_return(status: 200, body: "stubbed", headers: {})
+  #WebMock.stub_request(:any, "https://api.twitter.com").to_return(status: 200, body: "stubbed", headers: {})
 
   before :each do 
     @u = FactoryGirl.create(:user)
-    p = FactoryGirl.attributes_for(:post)
-    @u.posts.create(p)
-    @post = @u.posts.first
+    @post = @u.posts.create(FactoryGirl.attributes_for(:post))
   end
 
-  it "creates a tweet when it is posted" do 
-   expect(@post.tweet).to be_a Tweet
-  end
+  context "non-reply post" do 
 
-  context "replies" do 
-    before :each do 
-      @post = @u.posts.first
-      reply_hash = FactoryGirl.attributes_for(:reply_post)
-      @new_post = @u.posts.create(reply_hash)
+    it "is not a reply" do 
+      expect(@post.reply?).to eq false
+    end
+
+    it "does not have a parent" do 
+      expect(@post.parent).to eq nil
+    end
+
+    it "does not have a parent tweet id " do 
+      expect(@post.parent_tweet_id).to eq nil
     end
   end
 
-  it "can be a reply to another post" do 
-    expect(@post.replies.first).to eq @new_post
-  end
+  context do "reply post" 
+    before :each do 
+      @child = @post.replies.create(FactoryGirl.attributes_for(:reply_post))
+    end
 
-  it "knows that it is a reply" do 
-    expect(@new_post.reply?).to eq true
+    it "can be a reply to another post" do 
+      expect(@post.replies.first).to eq @child
+    end
+
+    it "knows it is a reply" do 
+      expect(@child.reply?).to eq true
+    end
+
+    it "has a link to its parent" do 
+      expect(@child.parent).to eq @post
+    end
+
+    it "knows their parents tweet id" do 
+      @post.tweet.stub(:twitter_id) { 1 }
+      expect(@child.parent_tweet_id).to eq 1
+    end
   end
 end
